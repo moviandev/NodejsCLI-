6551556628ad1e6117ee4ffe37de7622d79a69bb
Matheus Viana <matheus_o_viana@hotmail.com>
2020-04-05 16:08:35 -0300

    feat: added first main command 'key'

--- bin/crcur.js
+++ bin/crcur.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
+const program = require('commander');
+const pkg = require('../package.json');
 
-console.log('Hello World')
+program
+  .version(pkg.version)
+  .command('key', `Manage API Key - 'https://npmics.com'`)
+  .parse(process.argv);
+
+console.log('Hello World');
