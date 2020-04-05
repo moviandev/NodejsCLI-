68ee454b4041b73caf89f84d9dc15ce8ecec5587
Matheus Viana <matheus_o_viana@hotmail.com>
2020-04-05 18:47:45 -0300

    feats: added nomics api to this project created formatter and made some fixes

--- commands/key.js
+++ commands/key.js
@@ -12,7 +12,7 @@ const key = {
         {
           type: 'input',
           name: 'key',
-          message: 'Enter API key'.green + ' https://nomics.com ',
+          message: 'Enter API key'.green + ' https://nomics.com'.italic.magenta,
           validate: isRequired,
         },
       ])
