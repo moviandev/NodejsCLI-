e61d60ab3a91d0ccdc02021746fe52cd42fd6639
Matheus Viana <matheus_o_viana@hotmail.com>
2020-04-05 17:31:41 -0300

    feats: added KeyManaganer and added set key config

--- commands/key.js
+++ commands/key.js
@@ -1,6 +1,30 @@
+const inquirer = require('inquirer');
+const colors = require('colors');
+const KeyManager = require('../lib/KeyManager');
+
+const keyManager = new KeyManager();
+
 const key = {
-  set() {
-    console.log('Hello from set');
+  async set() {
+    const input = await inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'key',
+          message: 'Enter API key'.green + 'https://nomics.com',
+          validate: isRequired,
+        },
+      ])
+      .catch((err) => {
+        throw new Error(
+          'Not possible to set a API Key, contact the maintainer',
+          err.message,
+        );
+      });
+
+    const key = keyManager.setKey(input.key);
+
+    if (key) console.log('API Key Set'.blue);
   },
   show() {
     console.log('Hello from show');
